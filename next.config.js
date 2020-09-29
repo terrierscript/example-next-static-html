bde2b5baaa17cd4d74df1eccf7e573e3d3b2d981
terrierscript <13282103+terrierscript@users.noreply.github.com>
2020-09-30 00:04:42 +0900

    add

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   target: 'serverless',
   exportTrailingSlash: true,
-  baseUrl: "https://terrierscript.github.io/example-next-static-html/"
+  basePath: "/example-next-static-html",
+  // assetPrefix: "/example-next-static-html/"
   // exportPathMap: async function (
   //   defaultPathMap,
   //   { dev, dir, outDir, distDir, buildId }
